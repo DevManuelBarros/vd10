a9aa000f30cea4676d878a4abf339ad7f1fbf4bd
DevManuelBarros <dev.manuel.barros@gmail.com>
2019-09-02 01:46:41 -0300

    Config2

--- vd/settings.py
+++ vd/settings.py
@@ -126,6 +126,8 @@ LOGOUT_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 STATICFILES_DIRS = [
     os.path.join(os.path.join(BASE_DIR), "static"),
 ]
