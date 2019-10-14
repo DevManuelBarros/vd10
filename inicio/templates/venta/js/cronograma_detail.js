100290d5d3e4e6ec4d3b2f9792a992c15e2b370e
DevManuelBarros <dev.manuel.barros@gmail.com>
2019-10-13 21:28:11 -0300

    13-10-Ubuntu4

---
+++ inicio/templates/venta/js/cronograma_detail.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+function cambiarEstado(){
+	alert("va bien");
+}
+
+</script>
--- inicio/templates/venta/js/cronograma_list.js
+++ inicio/templates/venta/js/cronograma_list.js
@@ -1,4 +1,3 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function(){
     var request = $.ajax({
@@ -13,26 +12,47 @@ $(document).ready(function(){
 });
 
 
-function filtroNombre() {
-	  var input, filter, table, tr, td, i, txtValue;
+function filtroNombreCliente() {
+	  //definimos todas las variables
+	  var input, filter, table, tr, td, i, txtValue, input2, filter2, td2, txtValue2;
+	  //levantamos los datos de nombre
 	  input = document.getElementById("filtroNombre");
 	  filter = input.value.toUpperCase();
+	  //levantamos los datos del cliente
+	  input2 = document.getElementById("clientes_select");
+	  input2 = input2.options[input2.selectedIndex].text;
+	  filter2 = input2.toUpperCase();
+	  //tomamos todos los datos de la tabla
 	  table = document.getElementById("tablaResultado");
 	  tr = table.getElementsByTagName("tr");
+	  //recorremos toda la tabla
 	  for (i = 0; i < tr.length; i++) {
-	    td = tr[i].getElementsByTagName("td")[0];
-	    if (td) {
+	    //Tomamos cada columna necesaria
+		td = tr[i].getElementsByTagName("td")[0];
+	    td2 = tr[i].getElementsByTagName("td")[2];
+	    //Si de los dos campos obtenemos algo continua la comprabacion
+	    if (td && td2) {
+	      //Obtenemos los valores segun corresponda.
 	      txtValue = td.textContent || td.innerText;
-	      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-	        tr[i].style.display = "";
-	      } else {
-	        tr[i].style.display = "none";
+	      txtValue2 = td2.textContent || td2.innerText;
+	      //Si el select contiene valores vacios solo se filtra por el TextBox
+	      if (filter2 == '---------') {
+	    	  if (txtValue.toUpperCase().indexOf(filter) > -1){
+	    		  tr[i].style.display = "";
+		      } else {
+		        tr[i].style.display = "none";
+		      }
+
+	      }else{
+	    	      // En caso contrario filtramos por los dos valores.
+			      if (txtValue.toUpperCase().indexOf(filter) > -1 && txtValue2.toUpperCase().indexOf(filter2) > -1) {
+			        tr[i].style.display = "";
+			      } else {
+			        tr[i].style.display = "none";
+			      }
 	      }
 	    }       
 	  }
 	}
-	
-function filtroCliente(){
-	
-}
+
 </script>
