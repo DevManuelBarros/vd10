fe0568f406645a6aa956a9461ea892c05fe596ce
Manuel Barros <dev.manuel.barros@gmail.com>
2019-10-21 15:48:24 -0300

    21-10-2019Winp

--- inicio/templates/venta/js/cronograma_list.js
+++ inicio/templates/venta/js/cronograma_list.js
@@ -32,40 +32,91 @@
 		  //tomamos todos los datos de la tabla
 		  table = document.getElementById("tablaResultado");
 		  tr = table.getElementsByTagName("tr");
+		 
+		
+		      
+		      
+		
+		 
 		  //recorremos toda la tabla
-		  for (i = 0; i < tr.length; i++) {
-		    //Tomamos cada columna necesaria
-			td = tr[i].getElementsByTagName("td")[0];
-		    td2 = tr[i].getElementsByTagName("td")[3];
-		    td3 = tr[i].getElementsByTagName("td")[4];
+		 for (i = 0; i < tr.length; i++) 
+		 {
+			 //Tomamos cada columna necesaria
+			 td = tr[i].getElementsByTagName("td")[0];
+			 td2 = tr[i].getElementsByTagName("td")[3];
+			 td3 = tr[i].getElementsByTagName("td")[4];
+			 
 		    
 		    //Si de los dos campos obtenemos algo continua la comprabacion
-		    if (td && td2) {
-		      //Obtenemos los valores segun corresponda.
-		      txtValue = td.textContent || td.innerText;
-		      txtValue2 = td2.textContent || td2.innerText;
-		      //Si el select contiene valores vacios solo se filtra por el TextBox
-		      	if (filter2 == '---------') 
-		      	{
-		    	  	if (txtValue.toUpperCase().indexOf(filter) > -1)
-		    	  {
-		    		  tr[i].style.display = "";
-			      } else {
-			        tr[i].style.display = "none";
-			      }
-	
-		    	}
-		    else
+		    if (td && td2 && td3) 
 		    {
-		    	// En caso contrario filtramos por los dos valores.
-				      if (txtValue.toUpperCase().indexOf(filter) > -1 && txtValue2.toUpperCase().indexOf(filter2) > -1) {
-				        tr[i].style.display = "";
-				      } else {
-				        tr[i].style.display = "none";
-				      }
+		      //Obtenemos los valores segun corresponda.
+		      txtValue  = td.textContent  || td.innerText;
+		      txtValue2 = td2.textContent || td2.innerText;
+		      txtValue3 = td3.textContent || td3.innerText;
+		      
+
+		      if(txtValue3.toUpperCase().indexOf('FALSE') > -1)
+		      {
+
+		        txtValue3 = 'ACTIVAS';
 		      }
-		    }       
-		  }
+		      else
+		      {
+		      	txtValue3 = 'TERMINADAS';
+		      }
+		      //Sumamos las condiciones para ver que tipo de filtro tenemos:
+		      // filtro3 = 3 filtro2 = 2.
+		      // A partir de esos valores sumamos: 
+		      // si condicional es 0 = filtro1
+		      // si condicional es 2 = filtro1 y filtro2
+		      // si condicional es 3 = filtro1 y filtro3
+		      // si condicional es 5 = filtro1 filtro2 y filtro3
+		      var condicional = filter3 != 'TODOS' ? 3 : 0;
+		      condicional += filter2 == '---------' ? 0 : 2;
+		      
+		      switch (condicional) 
+		      {
+		      		case 0:
+		      			if (txtValue.toUpperCase().indexOf(filter) > -1)
+		      			{
+		      				tr[i].style.display = "";
+				     	} 
+				     	 else 
+				     	{
+				       		tr[i].style.display = "none";
+				     	}
+				     	break;
+				    case 2:
+				    	if (txtValue.toUpperCase().indexOf(filter) > -1 && txtValue2.toUpperCase().indexOf(filter2) > -1 ) 
+				      	{
+				        	tr[i].style.display = "";
+				      	} 
+				      	else {
+				        	tr[i].style.display = "none";
+				      	}
+				      	break;
+				    case 3:
+				    	if (txtValue.toUpperCase().indexOf(filter) > -1 && txtValue3.toUpperCase().indexOf(filter3) > -1 ) 
+				      	{
+				        	tr[i].style.display = "";
+				      	} 
+				      	else {
+				        	tr[i].style.display = "none";
+				      	}
+				      	break;
+				    case 5:
+				   		 if (txtValue.toUpperCase().indexOf(filter) > -1 && txtValue2.toUpperCase().indexOf(filter2) > -1 && txtValue3.toUpperCase().indexOf(filter3) > -1 ) 
+				      	{
+				        	tr[i].style.display = "";
+				      	} 
+				      	else {
+				        	tr[i].style.display = "none";
+				      	}
+		      }
+		    }     
+
+		}
 	}
 
 </script>
